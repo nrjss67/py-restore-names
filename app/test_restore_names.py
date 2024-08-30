8079682a627fae59236275c58d97466c380b493f
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-30 13:10:42 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,31 +6,31 @@ from app.restore_names import restore_names
     "user,expected",
     [
         (
-            {
-                "first_name": None,
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-              },
-            {
-                "first_name": "Jack",
-                "last_name": "Holy",
-                "full_name": "Jack Holy",
-              },
+                {  # noqa
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                },
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",  # noqa
+                },
         ),
         (
-            {
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-              },
-            {
-                "first_name": "Mike",
-                "last_name": "Adams",
-                "full_name": "Mike Adams",
-              },
+                { # noqa
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                },
         )
     ]
 )
-def test_restore_first_name_or_last_name_if_that_not_exist(user, expected): # noqa
+def test_restore_first_name_or_last_name_if_that_not_exist(user, expected):  # noqa
     list_users_dict = [user]
     restore_names(list_users_dict)
     assert list_users_dict[0] == expected
